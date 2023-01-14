f58e0254df21ad231a10ed11fbb9cbdeef151203
Premc3 <117389287+Premc3@users.noreply.github.com>
2023-01-14 17:26:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import Photos from './Components/Photos';
+
+class App extends Component {
+  render() {
+    return (
+    <Photos/>
+    )
+  }
+}
+
+export default App;
